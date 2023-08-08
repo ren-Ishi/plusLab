4363e05be405f66675a063191cbcdcb4fdf8dd91
Tenn-dazo <tenn.0619acc@gmail.com>
2023-08-09 06:45:42 +0900

    404 test05

--- js/main.js
+++ js/main.js
@@ -3,12 +3,25 @@ async function fetchData() {
     const response = await fetch('db.json');
     const jsonData = await response.json();
     
-    const dataContainer = document.getElementById('data-container');
+    const newpostBox = document.getElementById('newpost-box');
+    const newPosts = jsonData.data.slice(-4);
 
-    jsonData.data.forEach(item => {
-      const dataElement = document.createElement('div');
-      dataElement.innerHTML = `ID: ${item.id}, Name: ${item.name}`;
-      dataContainer.appendChild(dataElement);
+    newPosts.forEach(item => {
+      const postP = document.createElement('p');
+      const postDiv = document.createElement('div');
+      const postImg = document.createElement('img');
+      const postTag = document.createElement('h5');
+      const tagsString = item.tag.map(tag => `#${tag}`).join(' ');
+
+      postDiv.classList.add('post-div');
+      postImg.classList.add('post-img');
+      postP.innerHTML = item.title;
+      postTag.innerHTML = tagsString;
+
+      postDiv.appendChild(postImg);
+      postDiv.appendChild(postP);
+      postDiv.appendChild(postTag);
+      newpostBox.appendChild(postDiv);
     });
   } catch (error) {
     console.error('データの取得中にエラーが発生しました', error);
